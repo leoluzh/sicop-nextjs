63719093fe9efcfee17110d44afa6fae2b74d0b8
leonardo luz fernandes <leonardo.l.fernandes@gmail.com>
2023-03-30 00:20:24 -0300

    Inicialização do projeto SICOP utilizando NextJS

---
+++ pages/pages/empty/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const EmptyPage = () => {
+    return (
+        <div className="grid">
+            <div className="col-12">
+                <div className="card">
+                    <h5>Página Vazia</h5>
+                    <p>Utilize usar página como uma referência para criar seu conteúdo customizado.</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default EmptyPage;
